e6e992f94751b46cfd55ff31be05ca8b5734a4da
david monir <34280997+davidmonir17@users.noreply.github.com>
2022-07-03 16:54:07 +0200

    add singleton instance

---
+++ samuraiApp/singeleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace singeleton
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            counter counter1 = counter.getInstance();
+            counter counter2 = counter.getInstance();
+            counter1.AddOne();
+            counter2.AddOne();
+            counter1.AddOne();
+
+            Console.WriteLine("counter1:" + counter1.count);
+            Console.WriteLine("counter2:" + counter2.count);
+        }
+    }
+}
---
+++ samuraiApp/singeleton/counter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singeleton
+{
+    public class counter
+    {
+        public int count = 0;
+        private static counter instance = null;
+
+        public static counter getInstance()
+        {
+            if (instance == null) { instance = new counter(); }
+            return instance;
+        }
+
+        private counter()
+        {
+        }
+
+        public void AddOne()
+        { count++; }
+    }
+}
